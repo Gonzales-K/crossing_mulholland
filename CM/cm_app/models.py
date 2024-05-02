8e05f7dce9a7754a48c3a9627832345b11a0ba4e
Krystin <krystin.n.gonzales@gmail.com>
2024-05-02 13:19:50 -0700

    first migration

--- CM/cm_app/models.py
+++ CM/cm_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Collection(models.Model):
+    collection_name = models.CharField(max_length = 20)
+
+class Item(models.Model):
+    name = models.CharField(max_length = 20)
+    fabric = models.CharField(max_length = 20)
+    care_instructions = models.TextField()
+    price = models.PositiveSmallIntegerField()
+    collection = models.ForeignKey(
+        Collection,
+        related_name = 'item',
+        on_delete = models.CASCADE,
+    )
