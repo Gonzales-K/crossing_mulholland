f375e897c42650d3c3d51be3a7ea76ab3e350c65
Krystin <krystin.n.gonzales@gmail.com>
2024-06-04 11:34:35 -0700

    Added cart model, modified item model picture field

--- CM_project/CM_app/admin.py
+++ CM_project/CM_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Collection, Care, Item
+from .models import Collection, Care, Item, Cart, CartItem
 
 # Register your models here.
 admin.site.register(Collection)
 admin.site.register(Care)
 admin.site.register(Item)
+admin.site.register(Cart)
+admin.site.register(CartItem)
--- CM_project/CM_app/models.py
+++ CM_project/CM_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
 class Collection(models.Model):
@@ -11,7 +13,7 @@ class Care(models.Model):
 class Item(models.Model):
     name = models.CharField(max_length = 20)
     price = models.PositiveSmallIntegerField()
-    picture = models.ImageField()
+    picture = models.ImageField(upload_to='img', default='')
     fabric = models.ForeignKey(
         Care,
         related_name= 'item',
@@ -22,3 +24,29 @@ class Item(models.Model):
         related_name = 'item',
         on_delete = models.CASCADE
     )
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    user = models.ForeignKey(
+        User,
+        on_delete= models.CASCADE
+        )
+    completed = models.BooleanField(default=False)
+    def __str__(self):
+        return str(self.id)
+
+class CartItem(models.Model):
+    commodity = models.ForeignKey(
+        Item,
+        related_name='cart_commodities',
+        on_delete = models.CASCADE
+    )
+    cart = models.ForeignKey(
+        Cart,
+        related_name='cart_item',
+        on_delete= models.CASCADE
+    )
+    quantity = models.PositiveSmallIntegerField(default=0)
+    
